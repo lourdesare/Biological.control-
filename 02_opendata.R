6d5db54b38662ff9461c30b5bfb51250a9ecbe3d
Lourdes Arellano <lsarellano@outlook.com>
2021-11-23 14:19:53 +0100

    tables update

--- 02_opendata.R
+++ 02_opendata.R
@@ -9,8 +9,13 @@ AntagonisticActivity <-
   read_excel("data/peerj-55829-Raw_data.xls",
              sheet = "Antagonistic activity",
              range = "A3:E38")
-DiseaseIncidenceAndSeverity <- read_excel("data/peerj-55829-Raw_data.xls",sheet = "disease incidence and severity")
-MorphometricCharactOfPotato <- read_excel("data/peerj-55829-Raw_data.xls",sheet = "morphometric charact of potato")
+DiseaseIncidenceAndSeverity <- read_excel("data/peerj-55829-Raw_data.xls",
+                                          sheet = "disease incidence and severity",
+                                          range = "A4:N40")
+MorphometricCharactOfPotato <- read_excel("data/peerj-55829-Raw_data.xls",
+                                          sheet = "morphometric charact of potato",)
 BiomassDistribution <- read_excel("data/peerj-55829-Raw_data.xls",sheet = "biomass distribution")
-YieldOfPotato <- read_excel("data/peerj-55829-Raw_data.xls",sheet = "yield of potato")
+YieldOfPotato <- read_excel("data/peerj-55829-Raw_data.xls",
+                            sheet = "yield of potato",
+                            range = "B3:I15")
 
