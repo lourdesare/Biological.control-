7e86f85e7f8426fbd24991a770627b80a6e7db34
Antonia <antonia_finkler02@hotmail.com>
2021-12-14 10:01:48 -0300

    ultimo

--- 03_analysis_DataCultivars.R
+++ 03_analysis_DataCultivars.R
@@ -52,51 +52,57 @@ ggplot(Data.Cultivars.Final, aes(x=`Weeks after planting`, y=Stems, fill=Treatme
   facet_grid(.~Year)+
   theme_light()
 
+
+
 ########### DUNCAN TEST
 
 d1 <- Data.Cultivars.Final[Data.Cultivars.Final$Year==year & Data.Cultivars.Final$`Weeks after planting`==week, ]
 
 ## Plant Height
+Y <- W <- Tr <- L <- numeric()
 
-model <- aov(Plant.Height~Treatment, data=d1)
-summary(model)
-DT <- duncan.test(model, "Treatment", console = TRUE)
+#i <- 2015
+#j <- levels(Data.Cultivars.Final$`Weeks after planting`)[1]
 
 for (i in 2015:2016) { 
   for(j in levels(Data.Cultivars.Final$`Weeks after planting`)) {
     year <- i
   week <- j
-  source("duncan.R")}
+  model <- aov(Plant.Height~Treatment, data=d1)
+  summary(model)
+  DT <- duncan.test(model, "Treatment", console = TRUE)
+  Y <- c(Y, i,i,i)
+  W <- c(W, j,j,j)
+  Tr <- c(Tr, row.names(DT$groups))
+  L <- c(L, DT$groups$groups)
+  } 
 }
 
+LettersPH <- data.frame(year = Y, `Weeks after planting` = factor(W), Treatment = factor(Tr), letra = L )
 
 ## Disease Incidence
 
-for (i in 2015:2016) { 
-  for(j in levels(Data.Cultivars.Final$`Weeks after planting`)) {
-    year <- i
-    week <- j
-    source("duncan.R")}
-}
+
 
 ## Disease Severity
 
-for (i in 2015:2016) { 
-  for(j in levels(Data.Cultivars.Final$`Weeks after planting`)) {
-    year <- i
-    week <- j
-    source("duncan.R")}
-}
+
 
 ## Stems
 
-for (i in 2015:2016) { 
-  for(j in levels(Data.Cultivars.Final$`Weeks after planting`)) {
-    year <- i
-    week <- j
-    source("duncan.R")}
-}
 
 
+#### Juntar letras en la grafica 
 
+# Plant Height
+
+ggplot(Data.Cultivars.Final, aes(x=`Weeks after planting`, y=Plant.Height, fill=Treatment))+
+  geom_boxplot()+
+  facet_grid(.~Year)+
+  theme_light()+
+  geom_text(data = LettersPH,
+            mapping = aes(x = Weeks.after.planting,
+                          y = 45,
+                          label = letra))
+            
 
