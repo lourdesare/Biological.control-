572a127e3d3950460f9d413672a4fa77f5f782a0
Antonia <antonia_finkler02@hotmail.com>
2021-12-16 09:25:57 -0300

    AHORA SI

--- 03_analysis_DataCultivars.R
+++ 03_analysis_DataCultivars.R
@@ -58,7 +58,7 @@ ggplot(Data.Cultivars.Final, aes(x=`Weeks after planting`, y=Stems, fill=Treatme
 
 ## Plant Height
 
-Y <- W <- Tr <- L <- C <- numeric()
+Y <- W <- Tr <- L <- C <- M <- numeric()
 
 for (i in 2015:2016) { 
   for (k in levels(as.factor(Data.Cultivars.Final$Cultivar))) {
@@ -74,39 +74,17 @@ for (i in 2015:2016) {
   C <- c(C, k,k,k)
   Tr <- c(Tr, row.names(DT$groups))
   L <- c(L, DT$groups$groups)
+  M <- c(M, DT$groups$Plant.Height)
   } 
   }
 }
 
 
-LettersPH <- data.frame(year = Y, `Weeks after planting` = factor(W), 
-                        Cultivar = C, Treatment = factor(Tr), letra = L )
+LettersPH <- data.frame(Year = Y, `Weeks after planting` = factor(W), 
+                        Cultivar = C, Treatment = factor(Tr), letra = L, mea = M)
 
 ## Disease Incidence
 
-Y <- W <- Tr <- L <- C <- numeric()
-
-for (i in 2015:2016) { 
-  for (k in levels(as.factor(Data.Cultivars.Final$Cultivar))) {
-    for(j in levels(Data.Cultivars.Final$`Weeks after planting`)) {
-      d1 <- Data.Cultivars.Final[Data.Cultivars.Final$Year==i & 
-                                   Data.Cultivars.Final$`Weeks after planting`==j &
-                                   Data.Cultivars.Final$Cultivar==k, ]
-      model <- aov(Disease.Incidence~Treatment, data=d1)
-      summary(model)
-      DT <- duncan.test(model, "Treatment", console = TRUE)
-      Y <- c(Y, i,i,i)
-      W <- c(W, j,j,j)
-      C <- c(C, k,k,k)
-      Tr <- c(Tr, row.names(DT$groups))
-      L <- c(L, DT$groups$groups)
-    } 
-  }
-}
-
-
-LettersDI <- data.frame(year = Y, `Weeks after planting` = factor(W), 
-                        Cultivar = C, Treatment = factor(Tr), letra = L )
 
 
 ## Disease Severity
@@ -128,43 +106,19 @@ ggplot(Data.Cultivars.Final, aes(x=`Weeks after planting`, y=Plant.Height, fill=
   theme_light()+
   geom_text(data = LettersPH,
             mapping = aes(x = Weeks.after.planting,
-                          y = 45,
+                          y = mea + 5,
                           label = letra),
             position = position_dodge(0.9))
             
 
-# Disease Incidence
+## Disease Incidence
 
-ggplot(Data.Cultivars.Final, aes(x=`Weeks after planting`, y=Disease.Incidence, fill=Treatment))+
-  geom_boxplot()+
-  facet_grid(Cultivar~Year)+
-  theme_light()+
-  geom_text(data = LettersDI,
-            mapping = aes(x = Weeks.after.planting,
-                          y = 112,
-                          label = letra),
-            position = position_dodge(0.9))
 
-# Disease Severity
 
-ggplot(Data.Cultivars.Final, aes(x=`Weeks after planting`, y=Disease.Severity, fill=Treatment))+
-  geom_boxplot()+
-  facet_grid(Cultivar~Year)+
-  theme_light()+
-  geom_text(data = LettersDS,
-            mapping = aes(x = Weeks.after.planting,
-                          y = 60,
-                          label = letra),
-            position = position_dodge(0.9))
+## Disease Severity
 
-# Stems
 
-ggplot(Data.Cultivars.Final, aes(x=`Weeks after planting`, y=Stems, fill=Treatment))+
-  geom_boxplot()+
-  facet_grid(Cultivar~Year)+
-  theme_light()+
-  geom_text(data = LettersST,
-            mapping = aes(x = Weeks.after.planting,
-                          y = 10,
-                          label = letra),
-            position = position_dodge(0.9))
+
+## Stems
+
+
