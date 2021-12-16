5cbfa6cece4ab8f94f58faf4731b099515712cbd
Lourdes Arellano <lsarellano@outlook.com>
2021-12-16 11:17:56 +0100

    hola

--- 03_analysis_YieldFinal.R
+++ 03_analysis_YieldFinal.R
@@ -1,39 +1,61 @@
-#Yield duncan
-library(agricolae)
-model<-aov(Yield.Potato~Treatment,data=YieldOfPotato)
+
+
+#Duncan Yield 2015 Svitanok kievskiy
+YieldOfPotato$Cultivar <- factor(YieldOfPotato$Cultivar)
+Con <- YieldOfPotato$Cultivar == "Svitanok kievskiy" & YieldOfPotato$Year==2015
+d1 <- YieldOfPotato[Con,]
+
+model<-aov(Yield.Potato~Treatment,data=d1)
+out <- duncan.test(model,"Treatment", 
+                   main="Yield of potato")
+plot(out,variation="IQR")
+out
+
+#Duncan Yield 2015 Yuna
+YieldOfPotato$Cultivar <- factor(YieldOfPotato$Cultivar)
+Con1 <- YieldOfPotato$Cultivar == "Yuna" & YieldOfPotato$Year==2015
+d2 <- YieldOfPotato[Con1,]
+
+model<-aov(Yield.Potato~Treatment,data=d2)
+out <- duncan.test(model,"Treatment", 
+                   main="Yield of potato")
+plot(out,variation="IQR")
+out
+
+#Duncan Yield 2016 Svitanok kievskiy
+YieldOfPotato$Cultivar <- factor(YieldOfPotato$Cultivar)
+Con1234 <- YieldOfPotato$Cultivar == "Svitanok kievskiy" & YieldOfPotato$Year==2016
+d1234 <- YieldOfPotato[Con1234,]
+
+model<-aov(Yield.Potato~Treatment,data=d1234)
+out <- duncan.test(model,"Treatment", 
+                   main="Yield of potato")
+plot(out,variation="IQR")
+out
+
+#Duncan Yield 2016 Yuna
+YieldOfPotato$Cultivar <- factor(YieldOfPotato$Cultivar)
+Con12345 <- YieldOfPotato$Cultivar == "Yuna" & YieldOfPotato$Year==2016
+d2345 <- YieldOfPotato[Con12345,]
+
+model<-aov(Yield.Potato~Treatment,data=d2345)
 out <- duncan.test(model,"Treatment", 
                    main="Yield of potato")
 plot(out,variation="IQR")
 out
 #graph yield
-dp <- ggerrorplot(YieldOfPotato, x = "Cultivar", y = "Yield.Potato",
-                    color = "Treatment", palette = "Paired", 
-                    error.plot = "pointrange",
-                    position = position_dodge(0.5),xlab = (""), ylab = ("Yield of potato (ton/ha)"))+
+graphyield <- ggerrorplot(YieldOfPotato, x = "Cultivar", y = "Yield.Potato",
+                     color = "Treatment", palette = "Paired", 
+                     error.plot = "pointrange",
+                     position = position_dodge(0.5),xlab = ("Cultivars"), ylab = ("Yield of potato (t/ha)"))+
   facet_grid(.~Year)+
   theme_light()
-dp
+graphyield
 
-#Biomass duncan
-library(agricolae)
-model<-aov(Percentage.Biomass~Treatment,data=YieldOfPotato)
-out <- duncan.test(model,"Treatment", 
-                   main="Percentage of Biomass")
-plot(out,variation="IQR")
-out
-#graph yield
-dens <- ggerrorplot(YieldOfPotato, x = "Cultivar", y = "Percentage.Biomass",
-            color = "Treatment", palette = "Paired", 
-            error.plot = "pointrange",
-            position = position_dodge(0.5),xlab = ("Cultivars"), ylab = ("Biomass (%)"))+
-  facet_grid(.~Year)+
-  theme_light()
-dens
+
 #join two graphs 
 graphs <- plot_grid(
   dp + theme(legend.position="none"), dens + theme(legend.position="bottom"),
   labels = "AUTO", ncol = 1
 )
 graphs
-
-
