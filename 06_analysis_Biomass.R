9602cb5029badf8340152b1a735fd7f2365996b5
Lourdes Arellano <lsarellano@outlook.com>
2021-12-21 12:26:33 +0100

    upppdate

--- 06_analysis_Biomass.R
+++ 06_analysis_Biomass.R
@@ -1,14 +1,28 @@
-#Analysis Biomass.Distribution duncan
+
+
+#Prueba Biomass
+model<-aov(Percentage.Biomass~Treatment+`Fractions`+Year, data=BiomassDistribution)
+summary(model)
+
+BiomassDistribution$`Fractions` <- factor(BiomassDistribution$`Fractions`)
+
+library(ggplot2)
+ggplot(BiomassDistribution, aes(x=`Fractions`, y=Percentage.Biomass, fill=Treatment))+
+  geom_boxplot()+
+  facet_grid(Cultivar~Year)+
+  theme_light()
+
+##Biomass
 
 Y <- W <- Tr <- L <- C <- M <- numeric()
 
 for (i in 2015:2016) { 
   for (k in levels(as.factor(BiomassDistribution$Cultivar))) {
     for(j in levels(BiomassDistribution$`Fractions`)) {
-      d1 <- BiomassDistribution[BiomassDistribution$Year==i & 
+      d200 <- BiomassDistribution[BiomassDistribution$Year==i & 
                                    BiomassDistribution$`Fractions`==j &
                                    BiomassDistribution$Cultivar==k, ]
-      model <- aov(Percentage.Biomass~Treatment, data=d1)
+      model <- aov(Percentage.Biomass~Treatment, data=d200)
       summary(model)
       DT <- duncan.test(model, "Treatment", console = TRUE)
       Y <- c(Y, i,i,i)
@@ -16,35 +30,25 @@ for (i in 2015:2016) {
       C <- c(C, k,k,k)
       Tr <- c(Tr, row.names(DT$groups))
       L <- c(L, DT$groups$groups)
-      M <- c(M,DT$groups$Percentage.Biomass)
+      M <- c(M, DT$groups$Percentage.Biomass)
     } 
   }
 }
 
-#graph
-LettersBio <- data.frame(Year = Y, `Fractions` = factor(W), 
-                        Cultivar = C, Treatment = factor(Tr), MeanBiomass = M, letra = L )
-#
-model<-aov(Percentage.Biomass~Treatment+`Fractions`+Year, data=BiomassDistribution)
-summary(model)
+#Plant Height
+LettersPH1 <- data.frame(Year = Y, `Fractions` = factor(W), 
+                        Cultivar = C, Treatment = factor(Tr), letra = L, mea = M)
 
-BiomassDistribution$`Fractions` <- factor(BiomassDistribution$`Fractions`)
-
-library(ggplot2)
-plotbiomass <- ggplot(BiomassDistribution, aes(x=`Fractions`, y=Percentage.Biomass, fill=Treatment))+
+ggplot(BiomassDistribution, aes(x=`Fractions`, y=Percentage.Biomass, fill=Treatment))+
   geom_boxplot()+
   facet_grid(Cultivar~Year)+
   theme_light()+
-  geom_text(data = LettersBio,
+  geom_text(data = LettersPH1,
             mapping = aes(x = Fractions,
-                          y = MeanBiomass+7,
+                          y = mea + 5,
                           label = letra),
             position = position_dodge(0.9))+
   labs(caption = "Figure No. 1 Percentage of Biomass of potato") +
   theme(plot.caption.position = "plot",
         plot.caption = element_text(hjust = 0))
-plotbiomass 
-
-
-
   
