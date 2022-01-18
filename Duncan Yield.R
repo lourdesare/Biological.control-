49cd232450dfd96e118fac81acdca428a9af81cc
Antonia <antonia_finkler02@hotmail.com>
2022-01-18 06:40:56 -0300

    kruskal

--- Duncan Yield.R
+++ Duncan Yield.R
@@ -187,3 +187,71 @@ p4 <- plot(anova.yieldy6, main = "ANOVA 2016 y", which = 2, ask = F,sub.caption 
 
 
 
+#Kruskal Yield 2015 Svitanok kievskiy
+YieldOfPotato$Cultivar <- factor(YieldOfPotato$Cultivar)
+Con0 <- YieldOfPotato$Cultivar == "Svitanok kievskiy" & YieldOfPotato$Year==2015
+k1 <- YieldOfPotato[Con0,]
+
+kw <- kruskal(k1$Yield.Potato, k1$Treatment)
+kw$groups
+
+letterk <- character()
+letterk <- c(letterk,out1$groups$groups)
+
+
+#Kruskal Yield 2015 Yuna
+YieldOfPotato$Cultivar <- factor(YieldOfPotato$Cultivar)
+Con10 <- YieldOfPotato$Cultivar == "Yuna" & YieldOfPotato$Year==2015
+k2 <- YieldOfPotato[Con10,]
+
+kw <- kruskal(k2$Yield.Potato, k2$Treatment)
+kw$groups
+
+letterk <- c(letterk,out1$groups$groups)
+
+
+#Kruskal Yield 2016 Svitanok kievskiy
+YieldOfPotato$Cultivar <- factor(YieldOfPotato$Cultivar)
+Cont <- YieldOfPotato$Cultivar == "Svitanok kievskiy" & YieldOfPotato$Year==2016
+k3 <- YieldOfPotato[Cont,]
+
+kw <- kruskal(k3$Yield.Potato, k3$Treatment)
+kw$groups
+
+letterk <- c(letterk,out1$groups$groups)
+
+#Kruskal Yield 2016 Yuna
+YieldOfPotato$Cultivar <- factor(YieldOfPotato$Cultivar)
+Con12345 <- YieldOfPotato$Cultivar == "Yuna" & YieldOfPotato$Year==2016
+k4 <- YieldOfPotato[Con12345,]
+
+kw <- kruskal(k4$Yield.Potato, k4$Treatment)
+kw$groups
+
+letterk <- c(letterk,out1$groups$groups)
+
+#Kruskal Yield Graph 
+#Create data frame 
+dat_text <- data.frame(
+  label = letterk,
+  Year   = c(rep(2015,6),rep(2016,6)),
+  Treatment = rep(levels(as.factor(YieldOfPotato$Treatment)),4),
+  Cultivar= c("Svitanok kievskiy","Svitanok kievskiy","Svitanok kievskiy","Yuna","Yuna","Yuna","Svitanok kievskiy","Svitanok kievskiy","Svitanok kievskiy","Yuna","Yuna","Yuna"))
+
+kruskalyieldgraph <- ggplot(YieldOfPotato, aes(x=`Cultivar`, y=Yield.Potato, fill=Treatment))+ylab("Yield of potato(t/ha)")+
+  geom_boxplot()+
+  facet_grid(.~Year)+
+  theme_light()+
+  geom_text(data = dat_text,
+            mapping = aes(x = Cultivar,
+                          y = c(29.7, 23.8,21.8, 28,27,22,20.5,17.9,13.1,21,21.6,16.8),
+                          label = letter1),
+            position = position_dodge(0.9))+
+  labs(caption = "Figure No. 16 Results of Kruskal test Yield of potato") +
+  theme(plot.caption.position = "plot",
+        plot.caption = element_text(hjust = 0))
+kruskalyieldgraph
+
+
+
+
