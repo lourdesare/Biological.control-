ee12ffd734a56f43a7faf3b7163aaaac95403f68
Lourdes Arellano <lsarellano@outlook.com>
2022-01-18 11:23:19 +0100

    hola

--- 01_dowloaddata.R
+++ 01_dowloaddata.R
@@ -1,3 +1,4 @@
 if(!dir.exists("data")) dir.create("data")
 download.file("https://dfzljdn9uc3pi.cloudfront.net/2021/11578/1/peerj-55829-Raw_data.xls",
               "data/rawdata.xls")
+
--- duncan.R
+++
@@ -1,5 +0,0 @@
-d1 <- Data.Cultivars.Final[Data.Cultivars.Final$Year==year & Data.Cultivars.Final$`Weeks after planting`==week, ]
-
-model <- aov(Plant.Height~Treatment, data=d1)
-summary(model)
-DT <- duncan.test(model, "Treatment", console = TRUE)
